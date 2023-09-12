6f0291f2c421b43f82e7577813e43a734a3ec368
devtomasjr <gustavomatosvm@gmail.com>
2023-09-12 18:03:34 -0300

    quinto commit
    foi arrumado a tag da diferenciacao ficou correta,foi adicionado um video para feedback para pagina onde foi posto a tag <video> e source.

--- pages/index.js
+++ pages/index.js
@@ -11,106 +11,61 @@ export default function Home() {
       </Head>
 
       <main>
-
         <div className={styles.banner}>
-
           <h1 className={styles.title}>
             Impulsione suas vendas com páginas de vendas poderosas e eficazes!
           </h1>
-
-
-
-
-
         </div>
         <div classname={styles.subtitle}>
           <p>
             <strong>Aumente suas conversões e impulsione seus negócios com as melhores páginas de vendas do mercado</strong>
-
           </p>
-          <div>
-
-          </div>
         </div>
 
         <div className={`${styles.coluna} ${styles.solucao}`}>
           <p>
-            <strong> Você está cansado de ver suas oportunidades de venda escorregarem entre os dedos? Não perca mais tempo!</strong><br/>
-            Nossa equipe de especialistas em marketing digital desenvolveu as páginas de vendas mais eficazes e atraentes que vão alavancar seus resultados.
-
+            <strong> Você está cansado de ver suas oportunidades de venda escorregarem entre os dedos? Não perca mais tempo!</strong>
+            Nossa equipe de especialistas em marketing digital desenvolveu as páginas de vendas mais eficazes e atraentes que vão alavancar seus resultados. <br/>
           </p>
-          <p>
-            
-          </p>
-
           <img src='/aumento.jpg' alt="grafico" />
-
         </div>
-
-
         <div className={styles.coluna}>
-
           <p>
-
-            <strong> Nossas páginas de vendas são cuidadosamente projetadas para capturar a atenção do seu público-alvo e guiá-los em um processo de compra irresistível.</strong><br/>
+            <strong><span> Nossas páginas de vendas são cuidadosamente projetadas para capturar a atenção do seu público-alvo e guiá-los em um processo de compra irresistível.</span></strong><br/>
             <span> Com elementos estratégicos de persuasão e chamadas de ação convincentes, nossa copy cria uma experiência de compra envolvente, aumentando drasticamente suas taxas de conversão.</span>  
           </p>
-          
-            
-          <img className={[styles.conversaoC]} src="/coversao.jpg" alt="conversaoC"></img> 
-          
+          <img className={[styles.conversaoC]} src="/coversao.jpg" alt="conversaoC"></img>
           </div>
-        
-
         <div className={`${styles.diferenciacao} ${styles.coluna}`}>
-
           <p>
-
-
-            <strong><span> Não importa o tipo de produto ou serviço que você ofereça, nossas páginas de vendas personalizadas são feitas sob medida para o seu negócio.</span></strong>
-
+            <strong>Não importa o tipo de produto ou serviço que você ofereça, nossas páginas de vendas personalizadas são feitas sob medida para o seu negócio.</strong><br />
+            <span> Desde a construção de landing pages impressionantes até o desenvolvimento de textos persuasivos, lidamos com todos os detalhes para garantir que o seu negócio se destaque da concorrência.</span>
           </p>
-          <p>
-            Desde a construção de landing pages impressionantes até o desenvolvimento de textos persuasivos, lidamos com todos os detalhes para garantir que o seu negócio se destaque da concorrência.
-
-          </p>
-
+         
           <img src="/marcaR.jpg" alt="marcaregistrada"></img>
+          </div>
+          <div className='feedbacks'>
+            <h1>FEEDBECKS</h1>
+            <video width="640" height="360" controls>
+          <source src='/lead.mp4' alt='feedbacks'></source>
+          </video>
+
 
           </div>
-       
-
         <div className={[styles.chamada,styles.coluna]}>
-
           <p>
-
             <strong>Não perca mais tempo e dinheiro com páginas de vendas genéricas! Aumente seu ROI e conquiste mais clientes hoje mesmo com nossas páginas de vendas de alta performance.</strong><br/>
           </p>
-
-
-
-
-
         </div>
-
         <div className={[styles.depoimentos,styles.coluna]}>
           <strong>Entre em contato agora mesmo clicando em "Clique aqui" e descubra como podemos transformar sua presença online em vendas reais!</strong>
-
         </div>
-
         <div className={[styles.button,styles.coluna]}>
 
-
-          <a href='https://contate.me/paginasdvendas' target="_blank" title="whatsapp">  <button className='button'>Clique Aqui</button> </a>
-
-
-
-
-
+          <a href='https://contate.me/paginasdvendas' target="_blank" title="whatsapp">  
+          <img src='/Clique.png' alt='clique aqui'></img>
+          </a>
         </div>
-
-
-
       </main>
 
       <footer>
